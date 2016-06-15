ba5df4f93de6c131fa9ca75d0b082ae9e0fe5ec3
jasconchen <jasonchenzehui@gmail.com>
2016-06-15 17:11:48 +0800

    done H5ComponentPoyline

--- js/H5ComponentPolyline.js
+++ js/H5ComponentPolyline.js
@@ -28,10 +28,18 @@ var H5ComponentPoyline = function(name, cfg) {
 	}
 	// 垂直网格线（根据项目的个数去分）
 	step = cfg.data.length + 1;
+	var text_w = w / step >> 0;
 	for (var i = 0; i < step + 1; i++) {
 		var x = (w / step) * i;
 		ctx.moveTo(x, 0);
 		ctx.lineTo(x, h);
+
+		if (cfg.data[i]) {
+			var text = $('<div class="text"></div>');
+			text.text(cfg.data[i][0]);
+			text.css('width', text_w / 2).css('left', (x / 2 + text_w / 4));
+			component.append(text);
+		}
 	}
 
 	ctx.stroke();
@@ -43,7 +51,7 @@ var H5ComponentPoyline = function(name, cfg) {
 	cns.height = ctx.height = h;
 	component.append(cns);
 
-	// 绘制折现数据
+	// 绘制折线数据
 	ctx.beginPath();
 	ctx.lineWidth = 3;
 	ctx.strokeStyle = '#ff8878';
@@ -51,20 +59,45 @@ var H5ComponentPoyline = function(name, cfg) {
 	var x = 0;
 	var y = 0;
 
-	var row_w = (w / (cfg.data.length + 1))
+	var row_w = (w / (cfg.data.length + 1));
 	for (var i = 0; i < cfg.data.length; i++) {
 		var item = cfg.data[i];
 		console.log(item);
 
 		x = row_w * i + row_w;
-		y = h*(1-item[1]);
+		y = h * (1 - item[1]);
 		ctx.moveTo(x, y);
 		ctx.arc(x, y, 5, 0, 2 * Math.PI);
 	}
 	// 连线
 	// 移动画笔到第一个数据的点位置
-	ctx.moveTo(row_w, h*(1-cfg.data[0][1]));
-	ctx.arc(row_w, h*(1-cfg.data[0][1]), 20, 0, 2 * Math.PI);
+	ctx.moveTo(row_w, h * (1 - cfg.data[0][1]));
+	for (var i = 0; i < cfg.data.length; i++) {
+		var item = cfg.data[i];
+		x = row_w * i + row_w;
+		y = h * (1 - item[1]);
+		ctx.lineTo(x, y);
+	}
+	ctx.stroke();
+
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = 'rgba(255,255,255,0)';
+	// 绘制阴影
+	ctx.lineTo(x, h);
+	ctx.lineTo(row_w, h);
+	ctx.fillStyle = 'rgba(255,136,120,.2)';
+	ctx.fill();
+
+	// 写数据
+	for (var i = 0; i < cfg.data.length; i++) {
+		var item = cfg.data[i];
+		x = row_w * i + row_w;
+		y = h * (1 - item[1]);
+		if (item[2]) {
+			ctx.fillStyle = item[2] ? item[2] : '#595959';
+		}
+		ctx.fillText(((item[1] * 100) >> 0) + '%', x - 10, y - 10);
+	}
 
 	ctx.stroke();
 

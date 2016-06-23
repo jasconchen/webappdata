0a88d50d742e5144fd49dc7639424df36110c8d4
jasconchen <jasonchenzehui@gmail.com>
2016-06-23 21:35:51 +0800

    test test

--- js/H5ComponentPie.js
+++ js/H5ComponentPie.js
@@ -114,7 +114,9 @@ var H5ComponentPie = function(name, cfg) {
 		ctx.stroke();
 
 		if (per >= 1) {
+			component.find('.text').css('transition', 'all 0s');
 			H5ComponentPie.reSort(component.find('.text'));
+			component.find('.text').css('transition', 'all 1s');
 			component.find('.text').css('opacity', 1);
 			ctx.clearRect(0, 0, w, h);
 		}
@@ -155,19 +157,47 @@ H5ComponentPie.reSort = function(list) {
 		var offsetB = $(domB).offset();
 
 		// domA 的投影
-		var shaddowA_x = [offsetA.left, $(domA).width() + offsetA.left];
-		var shaddowA_y = [offsetA.top, $(domA).height() + offsetA.top];
+		var shadowA_x = [offsetA.left, $(domA).width() + offsetA.left];
+		var shadowA_y = [offsetA.top, $(domA).height() + offsetA.top];
 
 		// domB 的投影
-		var shaddowB_x = [offsetB.left, $(domB).width() + offsetB.left];
-		var shaddowB_y = [offsetB.top, $(domB).height() + offsetB.top];
+		var shadowB_x = [offsetB.left, $(domB).width() + offsetB.left];
+		var shadowB_y = [offsetB.top, $(domB).height() + offsetB.top];
 
 		// 检测 x
-		var intersect_x = shaddowA_x[0] > shaddowB_x[0] && shaddowA_x[0] > shaddowB_x[0]
+		var intersect_x = (shadowA_x[0] > shadowB_x[0] && shadowA_x[0] < shadowB_x[1]) || (shadowA_x[1] > shadowB_x[0] && shadowA_x[1] < shadowB_x[1]);
+		// 检测 y
+		var intersect_y = (shadowA_y[0] > shadowB_y[0] && shadowA_y[0] < shadowB_y[1]) || (shadowA_y[1] > shadowB_y[0] && shadowA_y[1] < shadowB_y[1]);
+
+		return intersect_x && intersect_y;
 	}
 
 	// 2.错开重排
 	var reset = function(domA, domB) {
+			if ($(domA).css('top') != 'auto') {
+				$(domA).css('top', parseInt($(domA).css('top')) + $(domB).height());
+			}
+			if ($(domA).css('bottom') != 'auto') {
+				$(domA).css('bottom', parseInt($(domA).css('bottom')) + $(domB).height());
+			}
 
+		}
+		// 定义将要重排的元素
+	var willReset = [list[0]];
+
+	$.each(list, function(i, domTarget) {
+
+		if (compare(willReset[willReset.length - 1], domTarget)) {
+			willReset.push(domTarget);
+		}
+
+	})
+	if (willReset.length > 1) {
+		$.each(willReset, function(i, domA) {
+			if (willReset[i + 1]) {
+				reset(domA, willReset[i + 1]);
+			}
+		})
+		H5ComponentPie.reSort(willReset);
 	}
 }
